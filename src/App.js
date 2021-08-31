f8346a7056bd579d35a6a5bd248d6cb4d56e18f1
rani-solanki <rani19@navgurukul.org>
2021-08-31 17:52:16 +0530

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // import logo from './logo.svg';
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route,} from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Landing from "./components/Landing";
+import Navbar from "./componants/Navbar";
+import Landing from "./componants/Landing";
 import "./App.css";
 
 function App() {
   return (
     <Fragment>
       <Router>
-        <Navbar />
+        <Navbar/>
         <Route exact path="/" component={Landing} />{" "}
       </Router>
     </Fragment>
