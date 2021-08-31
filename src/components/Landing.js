5cbfa232260d43d08494eca80d34792fa33fad38
rani-solanki <rani19@navgurukul.org>
2021-08-31 13:21:57 +0530

    First commit

---
+++ src/components/Landing.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+
+const Landing = () => {
+  return (
+    <Fragment>
+      <section className="landing">
+        <div className="landing-inner">
+          <h1 className="lead">
+            <h1> Rani Solanki </h1>
+            <p>
+              Developers who feel a personal sense of investment and pride in
+              the work that they do tend to be much more successful than the
+              ones who don’t.
+            </p>
+          </h1>
+        </div>
+      </section>
+    </Fragment>
+  );
+};
+
+export default Landing;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const Navbar = () => {
+    return (
+        <Fragment>
+            <div className="navbar">
+                <h1> Dashboard </h1>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Navbar; 
